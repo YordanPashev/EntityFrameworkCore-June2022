041026f674e869977413b41360c71d36fab345b2
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-13 23:28:51 +0300

    Adding Subject 01.ADO.NET with Demo code from Lecture

---
+++ 01.ADO.NET/ADO.NET - Lecture/Demo/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo
+{
+    public static class Config
+    {
+        public static string ConnectionString =
+             @"Server=DESKTOP-6A77MSO\SQLEXPRESS;Database=SoftUni;Integrated Security=true";
+    }
+}
---
+++ 01.ADO.NET/ADO.NET - Lecture/Demo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection= new SqlConnection(Config.ConnectionString);
+            sqlConnection.Open();
+
+            string employeeCountQuery = $"SELECT COUNT(*) AS [EmployeeCount]" +
+                              "FROM [Employees]";
+
+            SqlCommand employeeCountCommand = new SqlCommand(employeeCountQuery, sqlConnection);
+
+            int employeeCount = (int)employeeCountCommand.ExecuteScalar();
+
+            Console.WriteLine($"Total employees: {employeeCount}.");
+
+            string selectedJobTitle = Console.ReadLine();
+            string slsecEployeeByJObTItleQuery = @"SELECT [JobTitle],
+                                                          CONCAT([FirstName], ' ', LastName) AS [Full Name]
+                                                     FROM [Employees]
+                                                    WHERE [JobTitle] = @jobTitle";
+
+            SqlCommand eployeesByJobTitle = new SqlCommand(slsecEployeeByJObTItleQuery, sqlConnection);
+            eployeesByJobTitle.Parameters.AddWithValue("@jobTitle", selectedJobTitle);
+
+            using SqlDataReader employeeByJobTItleReader = eployeesByJobTitle.ExecuteReader();
+
+            string[] eployeesInfoByJobTitle = new string[employeeCount];
+            int emplyeeIndex = 0;
+
+            while (employeeByJobTItleReader.Read() == true)
+            {
+
+                string department = (string)employeeByJobTItleReader["JobTitle"];
+                string fullName = (string)employeeByJobTItleReader["Full Name"];
+
+                eployeesInfoByJobTitle[emplyeeIndex++] = $"#{emplyeeIndex + 1}. {department}  ->  {fullName}.";
+            }
+
+            foreach (string employee in eployeesInfoByJobTitle)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
