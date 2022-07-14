63bb7918c040a6d21f2090289ae1243702e1f776
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-14 14:54:04 +0300

    Adding ordering by name in the query for minions

--- 01.ADO.NET/ADO.NET - Exercise/P03.MinionNames/Queries/QueryForVillainMinions.sql
+++ 01.ADO.NET/ADO.NET - Exercise/P03.MinionNames/Queries/QueryForVillainMinions.sql
@@ -1,8 +1,9 @@
    SELECT m.[Name] AS [Minion Name],
-	  m.[Age] AS [Minion Age]
+		  m.[Age] AS [Minion Age]
      FROM [Villains] AS v
 LEFT JOIN [MinionsVillains] as mv
        ON v.[Id] = mv.[VillainId] 
 LEFT JOIN [Minions] AS m
        ON mv.[MinionId] = m.[Id]
     WHERE v.[Id] = @VillainId
+ ORDER BY m.[Name]
