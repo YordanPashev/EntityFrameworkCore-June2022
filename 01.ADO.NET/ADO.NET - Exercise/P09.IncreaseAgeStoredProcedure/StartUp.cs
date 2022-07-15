699a4a6ef38be61cf655235f8355636a26f2f495
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-15 15:23:23 +0300

    Adding Solution to P09

---
+++ 01.ADO.NET/ADO.NET - Exercise/P09.IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace P09.IncreaseAgeStoredProcedure
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int minionId = int.Parse(Console.ReadLine());
+
+            SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+            sqlConnection.Open();
+
+            try
+            {
+                string increadeMinionAgeQurey = @"EXEC dbo.[usp_IncreaseMinionsAgeByOne] @minionId";
+
+                SqlCommand increaseMinionsAgeCmd = new SqlCommand(increadeMinionAgeQurey, sqlConnection);
+                increaseMinionsAgeCmd.Parameters.AddWithValue("@MinionId", minionId);
+                SqlDataReader increaseMinionsAgeReader = increaseMinionsAgeCmd.ExecuteReader();
+
+                if (!increaseMinionsAgeReader.HasRows)
+                {
+                    throw new InvalidOperationException("The Miniond ID does not exist!");
+                }
+
+                increaseMinionsAgeReader.Read();
+ 
+                Console.WriteLine($"{increaseMinionsAgeReader["Name"]} - {increaseMinionsAgeReader["Age"]} years old");
+            }
+
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            sqlConnection.Close();
+        }
+    }
+}
