1730b88ba6cf652ca5b7ea0c565c9970ebfdb091
Yordan Pashev <92952122+YordanPashev@users.noreply.github.com>
2022-07-18 17:56:14 +0300

    Removing server info configurations

--- 02.ORM Fundamentals/ORM Fundamentals - Lecture/Data/Config.cs
+++ 02.ORM Fundamentals/ORM Fundamentals - Lecture/Data/Config.cs
@@ -3,6 +3,6 @@
     public class Config
     {
         public const string ConnectionString =
-            @"Server=DESKTOP-6A77MSO\SQLEXPRESS;Database=SoftUni2022;Integrated Security=true";
+            @"Server=.;Database=.;Integrated Security=true";
     }
 }
