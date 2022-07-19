00463a5e4f265a57d7a13e4924cd70d9dc7d28aa
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-19 19:19:06 +0300

    Adding Solution to P06. Adding a New Address and Updating Employee

---
+++ 03.Entity Framework Introduction/Entity Framework Introduction - Exercise/P06. Adding a New Address and Updating Employee/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftUni
+{
+
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string allEmployeesInfo = AddNewAddressToEmployee(softUniContext);
+            Console.WriteLine(allEmployeesInfo);
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext softUniContext)
+        {
+            Address newAddress = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee employeeWithLastNameNakov = softUniContext.Employees
+                                            .FirstOrDefault(e => e.LastName == "Nakov");
+            employeeWithLastNameNakov.Address = newAddress;
+
+            softUniContext.SaveChanges();
+
+            var employees = softUniContext.Employees
+                                .Select(e => new
+                                                {
+                                                    e.Address.AddressText,
+                                                    e.AddressId
+                                                })
+                                .OrderByDescending(e => e.AddressId)
+                                .Take(10)
+                                .ToArray();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                result.AppendLine($"{employee.AddressText}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
