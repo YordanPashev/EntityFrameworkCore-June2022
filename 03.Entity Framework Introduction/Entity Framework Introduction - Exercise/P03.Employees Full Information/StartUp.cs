0a650a12130fddc092e20c75b532717edaa2fdd6
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-19 16:03:55 +0300

    Adding Subject EF Core Introduction and Solution to P01 - 04 to Exercise

---
+++ 03.Entity Framework Introduction/Entity Framework Introduction - Exercise/P03.Employees Full Information/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace SoftUni
+{
+
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string allEmployeesInfo = GetEmployeesFullInformation(softUniContext);
+            Console.WriteLine(allEmployeesInfo);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var allEmployees = context.Employees
+                .Select(e => new
+                {
+                    e.EmployeeId,
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .OrderBy(e => e.EmployeeId)
+                .ToArray();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var employee in allEmployees)
+            {
+                result.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03.Entity Framework Introduction/Entity Framework Introduction - Exercise/P04. Employees with Salary Over 50 000/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftUni
+{
+
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using Microsoft.EntityFrameworkCore.Query;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string allEmployeesInfo = GetEmployeesWithSalaryOver50000(softUniContext);
+            Console.WriteLine(allEmployeesInfo);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var allEmployees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50_000)
+                .OrderBy(e => e.FirstName)
+                .ToArray();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var employee in allEmployees)
+            {
+                result.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
