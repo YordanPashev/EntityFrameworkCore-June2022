88d3ce71c90fa95053982345b9185c10474d4273
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-19 18:47:55 +0300

    Adding All Tasks Description and Solution to P05

---
+++ 03.Entity Framework Introduction/Entity Framework Introduction - Exercise/P05/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftUni
+{
+
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string allEmployeesInfo = GetEmployeesFromResearchAndDevelopment(softUniContext);
+            Console.WriteLine(allEmployeesInfo);
+        }
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            string nameOfTheDeparment = "Research and Development";
+            var allEmployeeesInCurrDeparment = context.Employees
+                .Where(e => e.Department.Name == nameOfTheDeparment)
+                .Select(e => new { 
+                                    e.FirstName, 
+                                    e.LastName,
+                                    e.Salary
+                                 })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .ToArray();
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (var employee in allEmployeeesInCurrDeparment)
+            {
+                result.AppendLine($"{employee.FirstName} {employee.LastName} from {nameOfTheDeparment} - ${employee.Salary:F2}");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
