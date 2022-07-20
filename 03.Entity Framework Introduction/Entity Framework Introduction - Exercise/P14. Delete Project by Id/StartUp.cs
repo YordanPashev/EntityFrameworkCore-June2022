091c08a28fa0431b90cc872a83549c433290e911
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-21 01:38:55 +0300

    Adding Solution to P14 nad P15

---
+++ 03.Entity Framework Introduction/Entity Framework Introduction - Exercise/P14. Delete Project by Id/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace SoftUni
+{
+
+    using System;
+    using System.Linq;
+    using System.Text;
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string allEmployeesInfo = DeleteProjectById(softUniContext);
+            Console.WriteLine(allEmployeesInfo);
+        }
+
+        public static string DeleteProjectById(SoftUniContext softUniContext)
+        {
+            int projectToRemoveId = 3;
+
+            Project projectToRemove = softUniContext.Projects
+                                            .FirstOrDefault(p => p.ProjectId == projectToRemoveId);
+
+            EmployeeProject[] employeeProjectsToRemove = softUniContext.EmployeesProjects
+                                            .Where(p => p.ProjectId == projectToRemoveId)
+                                            .ToArray();
+
+            softUniContext.EmployeesProjects.RemoveRange(employeeProjectsToRemove);
+            softUniContext.Projects.Remove(projectToRemove);
+            softUniContext.SaveChanges();
+
+            var result = new StringBuilder();
+            var projects = softUniContext.Projects
+                                                   .Take(10)
+                                                   .Select(p => p.Name)
+                                                   .ToArray();
+            foreach (var project in projects)
+            {
+                result.AppendLine($"{project}");            
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03.Entity Framework Introduction/Entity Framework Introduction - Exercise/P15. Remove Town/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace SoftUni
+{
+
+    using System;
+    using System.Linq;
+    using SoftUni.Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            SoftUniContext softUniContext = new SoftUniContext();
+            string allEmployeesInfo = RemoveTown(softUniContext);
+            Console.WriteLine(allEmployeesInfo);
+        }
+
+        public static string RemoveTown(SoftUniContext softUniContext)
+        {
+            Town townToRemove = softUniContext.Towns
+                                                .FirstOrDefault(t => t.Name == "Seattle");
+
+            Address[] addressesToRemove = softUniContext.Addresses
+                                            .Where(p => p.TownId == townToRemove.TownId)
+                                            .ToArray();
+
+            Employee[] employeesToSetNullForAddress = softUniContext.Employees
+                                                        .Where(e => e.Address.TownId == townToRemove.TownId)
+                                                        .ToArray();
+
+            foreach (var employee in employeesToSetNullForAddress)
+            {
+                employee.Address = null;
+            }
+
+            softUniContext.Addresses.RemoveRange(addressesToRemove);
+            softUniContext.Towns.Remove(townToRemove);
+            softUniContext.SaveChanges();
+
+            return $"{addressesToRemove.Count()} addresses in Seattle were deleted";
+        }
+    }
+}
