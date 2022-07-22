c27d0e57d9ac587d77d10850ff0653ffd9359793
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-22 18:36:25 +0300

    Adding Subject Entity Relations - Exercise with Solution to P01.StudentSystem

---
+++ 04. Entity Relations/Entity Relations - Exercise/P01.StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,29 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Resource
+    {
+        [Key]
+        public int ResourceId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(2000)")]
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        [ForeignKey(nameof(Course))]
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+
+    }
+}
---
+++ 04. Entity Relations/Entity Relations - Exercise/P01.StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,40 @@
+﻿namespace P01_StudentSystem.Data
+{
+
+    using Microsoft.EntityFrameworkCore;
+    using P01_StudentSystem.Data.Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext() { }
+
+
+        public StudentSystemContext(DbContextOptions<StudentSystemContext> options)
+            : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public DbSet<Resource> Resources { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-6A77MSO\\SQLEXPRESS;Database=StudentSystem;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>().HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
