56e291fffdd5a02d2369e4f4303988a5adadd1ea
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-23 23:58:24 +0300

    Adding Subject 05.LINQ and Demo core from Lecture

---
+++ 05.LINQ/LINQ - Lecture/SoftUni/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni;
+
+SoftUniContext context = new SoftUniContext();
+var resutl = context.Employees
+    .Include(e => e.Department)
+    .GroupBy(d => new 
+    {   DepartmentName = d.Department.Name, 
+        DeparmentID = d.DepartmentId
+    })
+    .Select(grp => new 
+    { 
+        grp.Key.DepartmentName,
+        DepartmentEmployees = grp.Select(de => de.FirstName)
+    })
+    .ToList();
+
+Console.WriteLine("");
