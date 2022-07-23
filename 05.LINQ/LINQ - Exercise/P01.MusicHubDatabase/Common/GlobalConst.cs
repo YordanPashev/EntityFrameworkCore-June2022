1bc4ce7c62e5ce68da05e043c74e428a9cc8af0b
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-24 01:07:53 +0300

    Adding All Task Description to Exercise and Solution to P01.MusicHubDase

---
+++ 05.LINQ/LINQ - Exercise/P01.MusicHubDatabase/Common/GlobalConst.cs
@@ -0,0 +1,26 @@
+﻿namespace MusicHub.Common
+{
+    public class GlobalConst
+    {
+        //Song
+        public const int SongNameMaxLength = 20;
+
+        //Album
+        public const int AlbumNameMaxLength = 40;
+
+        //Performer
+        public const int PerformerFirstNameMaxLength = 20;
+        public const int PerformerLastNameMaxLength = 20;
+
+        //Producer
+        public const int ProducerNameMaxLength = 30;
+        public const int ProducerPhoneNumberMaxLength = 20;
+
+
+        //Writer
+        public const int WriterNameMaxLength = 20;
+
+        //Global
+        public const int PseudonymMaxLength = 100;
+    }
+}
