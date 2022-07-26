8e901a5c606dc908f60d4d3c807bb46d40b894fb
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-26 17:37:33 +0300

    Adding Solution to P06 and 07

---
+++ 06.AdvancedQuerying/AdvancedQuerying - Exercise/P06.BookTitileByCategory/BookShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+            string categories = Console.ReadLine();
+            string result = GetBooksByCategory(db, categories);
+            Console.WriteLine(result);
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToLower())
+                .ToArray();
+            
+            string[] filteredBooks = context.BooksCategories
+                .Include(bc => bc.Category)
+                .Include(bc => bc.Book)
+                .OrderBy(b => b.Book.Title)
+                .Where(bc => categories.Contains(bc.Category.Name.ToLower()))
+                .Select(b => b.Book.Title)
+                .ToArray();
+             
+            StringBuilder output = new StringBuilder();
+
+            foreach (string bookTitle in filteredBooks)
+            {
+                output.AppendLine(bookTitle);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
