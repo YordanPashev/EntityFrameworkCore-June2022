6013c78d53166c9c0bfb1ac7be3373c29b038856
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-27 13:15:14 +0300

    Adding Solution to P15

---
+++ 06.AdvancedQuerying/AdvancedQuerying - Exercise/P15.IncreasePrice/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using System.Linq;
+    using BookShop.Initializer;
+    using BookShop.Models;
+    using Data;
+    using Z.EntityFramework.Plus;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+            IncreasePrices(db);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            IQueryable<Book> booksReleasedBefore2010 = context.Books
+                    .Where(b => b.ReleaseDate.Value.Year < 2010);
+
+            foreach (Book book in booksReleasedBefore2010)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+
+            ////Solution with Bulk
+            //context.Books
+            //    .Where(b => b.ReleaseDate.Value.Year < 2010)
+            //    .Update(p => new Book() { Price = p.Price + 5 });
+        }  
+    }
+}
