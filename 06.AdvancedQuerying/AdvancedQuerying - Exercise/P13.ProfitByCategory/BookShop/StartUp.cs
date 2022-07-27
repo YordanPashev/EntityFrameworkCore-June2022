940fe9df71a123f0630836a3e8362712d9645dc8
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-27 10:49:32 +0300

    Adding Solution to P13 and p14

---
+++ 06.AdvancedQuerying/AdvancedQuerying - Exercise/P13.ProfitByCategory/BookShop/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+            string auhorsBooksCopiesInfo = GetTotalProfitByCategory(db);
+            Console.WriteLine(auhorsBooksCopiesInfo);
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            var bookByTotalProfil = context.Categories
+                .Select(c => new
+                {
+                    CategoryName = c.Name,
+                    TotalProfit = c.CategoryBooks.Select(b => b.Book.Price * b.Book.Copies).Sum()
+                })
+                .OrderByDescending(c => c.TotalProfit)
+                .ThenBy(c => c.CategoryName)
+                .ToArray();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var booksInfo in bookByTotalProfil)
+            {
+                output.AppendLine($"{booksInfo.CategoryName} ${booksInfo.TotalProfit:F2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 06.AdvancedQuerying/AdvancedQuerying - Exercise/P14.MostRecentBooks/BookShop/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace BookShop
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+            string auhorsBooksCopiesInfo = GetMostRecentBooks(db);
+            Console.WriteLine(auhorsBooksCopiesInfo);
+        }
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            var cactegoriesInfo = context.Categories
+                .Include(cb => cb.CategoryBooks)
+                .ThenInclude(b => b.Book)
+                .OrderBy(c => c.Name)
+                .Select(c => new
+                {
+                    CategoryName = c.Name,
+                    TopThreeMostRecentBooks = c.CategoryBooks.Select(cb => new 
+                                                            { 
+                                                                cb.Book.Title, 
+                                                                cb.Book.ReleaseDate
+                                                            })
+                                                            .OrderByDescending(t => t.ReleaseDate)
+                                                            .Take(3)
+                })
+                .ToArray();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var category in cactegoriesInfo)
+            {
+                output.AppendLine($"--{category.CategoryName}");
+
+                foreach (var book in category.TopThreeMostRecentBooks)
+                {
+                    output.AppendLine($"{book.Title} ({book.ReleaseDate.Value.Year})");
+                }
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
