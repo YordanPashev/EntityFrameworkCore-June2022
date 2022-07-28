f6890f5f7af5a378a13deeee3a663e58ad9405fd
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-28 23:32:35 +0300

    Adding Fast Food Web Project

---
+++ 07.Auto Mapping Objects/Auto Mapping Objects - Exercise/FastFood.Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿namespace FastFood.Services.Interfaces
+{
+
+    using Models.Categories;
+
+    public interface ICategoryService
+    {
+        Task Add(CreateCategoryDTO categoryDTO);
+
+        Task<ICollection<ListCategoryDTOs>> GetAll();
+    }
+}
