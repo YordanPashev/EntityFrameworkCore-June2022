080f200e557f4b4a90b36c8559109d06a3c5dca3
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-28 10:49:05 +0300

    Adding Subject 07.Auto Mapping Objects and Demo Code from Lecture

---
+++ 07.Auto Mapping Objects/Auto Mapping Objects - Lecture/SoftUni/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using SoftUni;
+using SoftUni.Configurations;
+using SoftUni.Infrastructure.DTOs;
+
+SoftUniContext context  = new SoftUniContext();
+
+var config = new MapperConfiguration(c =>
+{
+    c.AddProfile<EmployeeMappingProfile>();
+});
+
+var mapper = config.CreateMapper();
+
+//Auto Mapping
+EmployeeDTO[] employeesInSelectedDepartment = context.Employees
+    .Where(e => e.DepartmentId == 4)
+    .ProjectTo<EmployeeDTO>(config)
+    .ToArray();
+
+foreach (var employee in employeesInSelectedDepartment)
+{
+    Console.WriteLine($"{employee.FullName} works in {employee.DepartmentName} as {employee.JobTitle}.");
+}
+
+
+////Manual Mapping
+//var employee = context.Employees
+//    .Where(e => e.EmployeeId == 1)
+//    .Select(e => new EmployeeDTO ()
+//    {
+//        FirstName = e.FirstName,
+//        LastName = e.LastName,
+//        JobTitle = e.JobTitle,
+//        Department = e.Department
+//    })
+//    .ToArray();
+
+//Console.WriteLine($"{employee.FullName} works in {employee.DepartmentName} as {employee.JobTitle}.");
+
+
