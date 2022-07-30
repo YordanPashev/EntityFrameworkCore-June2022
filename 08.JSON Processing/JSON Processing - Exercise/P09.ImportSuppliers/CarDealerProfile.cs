8de3e7ae342c0e826c539a3e5a110711fe3043fb
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-31 01:03:29 +0300

    Adding Solution to P09

---
+++ 08.JSON Processing/JSON Processing - Exercise/P09.ImportSuppliers/CarDealerProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace CarDealer
+{
+
+    using CarDealer.DTO.Supplier;
+    using CarDealer.Models;
+
+    using AutoMapper;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            this.CreateMap<ImportSupplierDTO, Supplier>();
+        }
+    }
+}
---
+++ 08.JSON Processing/JSON Processing - Exercise/P09.ImportSuppliers/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace CarDealer
+{
+
+    using System.IO;
+    using System.Collections.Generic;
+
+    using CarDealer.Data;
+    using CarDealer.Models;
+    using CarDealer.DTO.Supplier;
+
+    using AutoMapper;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            Mapper.Initialize(cfg => cfg.AddProfile(typeof(CarDealerProfile)));
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string filepath = Path.Combine(Directory.GetCurrentDirectory(), "../../../Datasets/suppliers.json"); 
+            string json = File.ReadAllText(filepath);
+
+            string result = ImportSuppliers(context, json);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            ImportSupplierDTO[] suppliersDTO = JsonConvert.DeserializeObject<ImportSupplierDTO[]>(inputJson);
+            Supplier[] suplliers = Mapper.Map<Supplier[]>(suppliersDTO);
+
+            context.Suppliers.AddRange(suplliers);
+            context.SaveChanges();
+
+            int addedSuppliersCount = suplliers.Length;
+            return $"Successfully imported {addedSuppliersCount}.";
+        }
+    }
+}
