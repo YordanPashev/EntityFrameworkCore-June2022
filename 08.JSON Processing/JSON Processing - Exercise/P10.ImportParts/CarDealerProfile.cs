5943f511c07122a48605762f9113dcb22195ee3a
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-07-31 11:52:34 +0300

    Adding Solution to P01

---
+++ 08.JSON Processing/JSON Processing - Exercise/P10.ImportParts/CarDealerProfile.cs
@@ -0,0 +1,19 @@
+﻿namespace CarDealer
+{
+    using CarDealer.Models;
+    using CarDealer.DTO.Supplier;
+    using CarDealer.DTO.Part;
+
+    using AutoMapper;
+
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Import Mappers
+            this.CreateMap<ImportSupplierDTO, Supplier>();
+
+            this.CreateMap<ImportPartDTO, Part>();
+        }
+    }
+}
---
+++ 08.JSON Processing/JSON Processing - Exercise/P10.ImportParts/StartUp.cs
@@ -0,0 +1,43 @@
+﻿namespace CarDealer
+{
+
+    using System.IO;
+    using System.Linq;
+
+    using CarDealer.Data;
+    using CarDealer.Models;
+    using CarDealer.DTO.Part;
+
+    using AutoMapper;
+    using Newtonsoft.Json;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+            Mapper.Initialize(cfg => cfg.AddProfile(typeof(CarDealerProfile)));
+
+            string filepath = ("../../../Datasets/parts.json"); 
+            string json = File.ReadAllText(filepath);
+
+            string result = ImportParts(context, json);
+            System.Console.WriteLine(result);
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            ImportPartDTO[] partsDTO = JsonConvert.DeserializeObject<ImportPartDTO[]>(inputJson);
+            Part[] parts = Mapper.Map<Part[]>(partsDTO.Where(s => IsSupplierIdExistInDb(context, s.SupplierId)));
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            int addingPatsCount = parts.Length;
+            return $"Successfully imported {addingPatsCount}.";
+        }
+
+        public static bool IsSupplierIdExistInDb(CarDealerContext context, int supplierID)
+            => context.Suppliers.Any(s => s.Id == supplierID);
+    }
+}
