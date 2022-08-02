182999b1b61555a0e24cf71feb93f7a8581e2746
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-08-02 18:46:15 +0300

    Adding Solution to P12 and 13

---
+++ 09.XML Processing/XML Processing -Exercise/P12.ImportCutomers/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    using XmlFacade;
+    using CarDealer.Data;
+    using CarDealer.Dtos.Import;
+    using CarDealer.Models;
+    
+    using AutoMapper;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string inputXml = File.ReadAllText("../../../Datasets/customers.xml");
+            string result = ImportCustomers(context, inputXml);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitialiseMapper();
+
+            string rootElementName = "Customers";
+            ImportCustomerDTO[] customersDTO = XMLConverter.Deserializer<ImportCustomerDTO>(inputXml, rootElementName);
+
+            Customer[] cutomers = mapper.Map <Customer[]>(customersDTO);
+
+            context.Customers.AddRange(cutomers);
+            context.SaveChanges();
+
+            int addedCustomersCount = customersDTO.Count();
+            return $"Successfully imported {addedCustomersCount}";
+        }
+
+        private static IMapper InitialiseMapper()
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+                cfg.AddProfile<CarDealerProfile>());
+
+            return config.CreateMapper();
+        }
+    }
+}
---
+++ 09.XML Processing/XML Processing -Exercise/P13.ImportSales/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace CarDealer
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    using XmlFacade;
+    using CarDealer.Data;
+    using CarDealer.Dtos.Import;
+    using CarDealer.Models;
+    
+    using AutoMapper;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string inputXml = File.ReadAllText("../../../Datasets/sales.xml");
+            string result = ImportSales(context, inputXml);
+            Console.WriteLine(result);
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitialiseMapper();
+
+            string rootElementName = "Sales";
+            ImportSaleDTO[] salesDTO = XMLConverter.Deserializer<ImportSaleDTO>(inputXml, rootElementName);
+
+            Sale[] sales = mapper.Map <Sale[]>(salesDTO.Where(s => context.Cars.Any(c => c.Id == s.CarId)));
+
+            context.Sales.AddRange(sales);
+            context.SaveChanges();
+
+            int addedSalesCount = sales.Count();
+
+            return $"Successfully imported {addedSalesCount}";
+        }
+
+        private static IMapper InitialiseMapper()
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+                cfg.AddProfile<CarDealerProfile>());
+
+            return config.CreateMapper();
+        }
+    }
+}
